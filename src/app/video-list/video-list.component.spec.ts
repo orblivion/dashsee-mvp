c0b8b27b2f754292247b938f2409f99ee437552d
Daniel Krol <orblivion@gmail.com>
2021-09-01 15:13:51 -0400

    Get all tests to pass again. Also allcaps DASH in DASHsee in header, which is in one of the tests

--- src/app/video-list/video-list.component.spec.ts
+++ src/app/video-list/video-list.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { VideoListComponent } from './video-list.component';
 
 describe('VideoListComponent', () => {
@@ -8,7 +8,8 @@ describe('VideoListComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ VideoListComponent ]
+      declarations: [ VideoListComponent ],
+      imports: [HttpClientTestingModule],
     })
     .compileComponents();
   });
--- src/app/video.service.spec.ts
+++ src/app/video.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { VideoService } from './video.service';
 
@@ -6,7 +7,9 @@ describe('VideoService', () => {
   let service: VideoService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(VideoService);
   });
 
