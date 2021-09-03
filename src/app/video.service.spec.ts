29d006cd5572d377a8f06b3d501907f2cdafd20d
Daniel Krol <orblivion@gmail.com>
2021-09-02 21:25:48 -0400

    More getVideo API errors

--- src/app/video.service.spec.ts
+++ src/app/video.service.spec.ts
@@ -1,7 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 
-import { VideoService } from './video.service';
+import { VideoService, VideoServiceError } from './video.service';
 import { of } from 'rxjs';
 
 describe('VideoService', () => {
@@ -13,52 +13,114 @@ describe('VideoService', () => {
     videoService = new VideoService(httpClientSpy as any)
   });
 
-  it('getVideo returns a video if the response is valid', (done) => {
-    httpClientSpy.post.and.returnValue(of({
-      'result': {
-        'Dash-Podcast-179': {
-          canonical_url: "lbry://@DigitalCashNetwork#c/Dash-Podcast-179#4",
-          short_url: "lbry://Dash-Podcast-179#4",
-          signing_channel: {
-            name: '@DigitalCashNetwork',
-            value: {
-              title: 'Digital Cash Network',
-              thumbnail: {
-                url: 'path/to/channel/thumbnail.png',
+  describe('getVideo', () => {
+    it('returns a video if the response is valid', (done) => {
+      httpClientSpy.post.and.returnValue(of({
+        result: {
+          'Dash-Podcast-179': {
+            canonical_url: "lbry://@DigitalCashNetwork#c/Dash-Podcast-179#4",
+            short_url: "lbry://Dash-Podcast-179#4",
+            signing_channel: {
+              name: '@DigitalCashNetwork',
+              value: {
+                title: 'Digital Cash Network',
+                thumbnail: {
+                  url: 'path/to/channel/thumbnail.png',
+                },
               },
             },
+            value: {
+              title: 'my title',
+              description: 'my description',
+              stream_type: 'video',
+              thumbnail: {
+                url: 'path/to/video/thumbnail.png',
+              },
+            },
+          }
+        }
+      }));
+
+      videoService.getVideo('Dash-Podcast-179').subscribe({
+        next: video => {
+          expect(video).toEqual({
+            title: "my title",
+            thumbnailUrl: "path/to/video/thumbnail.png",
+            description: "my description",
+            confirmedUri: "lbry://Dash-Podcast-179#4",
+            channel: {
+              handle: "@DigitalCashNetwork",
+              name: "Digital Cash Network",
+              thumbnailUrl: "path/to/channel/thumbnail.png",
+            },
+            canonicalUri: "@DigitalCashNetwork:c/Dash-Podcast-179:4",
+          })
+          expect(httpClientSpy.post.calls.mostRecent().args.length).toEqual(2)
+          expect(httpClientSpy.post.calls.mostRecent().args[1].params.urls).toEqual('Dash-Podcast-179')
+          done()
+        },
+        error: error => done.fail(error)
+      });
+    });
+
+    it('returns a "not found" if there is not the expected video in the response', (done) => {
+      httpClientSpy.post.and.returnValue(of({
+        result: {
+          'Dash-Podcast-179': {
+            error: {
+              name: 'NOT_FOUND',
+            }
           },
-          value: {
-            title: 'my title',
-            description: 'my description',
-            stream_type: 'video',
-            thumbnail: {
-              url: 'path/to/video/thumbnail.png',
+        }
+      }));
+
+      videoService.getVideo('Dash-Podcast-179').subscribe({
+        error: error => {
+          expect(error.type).toEqual(VideoServiceError.NotFound)
+          done()
+        },
+        next: x => done.fail
+      });
+    });
+
+    it('returns a "not video" if the steram is not a video', (done) => {
+      httpClientSpy.post.and.returnValue(of({
+        result: {
+          'Dash-Podcast-179': {
+            value: {
+              stream_type: 'audio',
             },
           },
         }
-      }
-    }));
+      }));
 
-    videoService.getVideo('Dash-Podcast-179').subscribe({
-      next: video => {
-        expect(video).toEqual({
-          title: "my title",
-          thumbnailUrl: "path/to/video/thumbnail.png",
-          description: "my description",
-          confirmedUri: "lbry://Dash-Podcast-179#4",
-          channel: {
-            handle: "@DigitalCashNetwork",
-            name: "Digital Cash Network",
-            thumbnailUrl: "path/to/channel/thumbnail.png",
+      videoService.getVideo('Dash-Podcast-179').subscribe({
+        error: error => {
+          expect(error.type).toEqual(VideoServiceError.NotVideo)
+          done()
+        },
+        next: x => done.fail
+      });
+    });
+
+    it('returns an unknown error for errors other than "not found"', (done) => {
+      httpClientSpy.post.and.returnValue(of({
+        result: {
+          'Dash-Podcast-179': {
+            error: {
+              name: 'SOMETHING_ELSE',
+            }
           },
-          canonicalUri: "@DigitalCashNetwork:c/Dash-Podcast-179:4",
-        })
-        expect(httpClientSpy.post.calls.mostRecent().args.length).toEqual(2)
-        expect(httpClientSpy.post.calls.mostRecent().args[1].params.urls).toEqual('Dash-Podcast-179')
-        done()
-      },
-      error: error => done.fail(error)
+        }
+      }));
+
+      videoService.getVideo('Dash-Podcast-179').subscribe({
+        error: error => {
+          expect(error.type).toEqual(VideoServiceError.Unknown)
+          done()
+        },
+        next: x => done.fail
+      });
     });
   });
 });
